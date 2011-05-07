83e378f37b895b022ce6c9f827a52517c2774a2d
Mike Thomsen <mikerthomsen@gmail.com>
2011-05-07 16:00:05 -0400

    Added L10N classes.

---
+++ plugins/UserProfiles/lib/UserProfiles/L10N/en_us.pm
@@ -0,0 +1,10 @@
+package UserProfiles::L10N::en_us;
+
+use strict;
+
+use base 'UserProfiles::L10N';
+use vars qw( %Lexicon );
+%Lexicon = ();
+
+1;
+
---
+++ plugins/UserProfiles/lib/UserProfiles/L10N/ru.pm
@@ -0,0 +1,15 @@
+package UserProfiles::L10N::ru;
+use strict;
+use UserProfiles::L10N;
+use UserProfiles::L10N::en_us;
+use vars qw( @ISA %Lexicon );
+@ISA = qw( UserProfiles::L10N::en_us );
+
+## The following is the translation table.
+
+%Lexicon = (
+
+);
+
+1;
+
